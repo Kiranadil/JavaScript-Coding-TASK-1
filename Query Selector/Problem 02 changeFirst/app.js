c07cfbb27cb38869528c456fed32ba42d5d3ae81
Kiranadil <kiranadil10@gmail.com>
2021-06-01 00:42:50 +0500

    changed the background color of button

--- Query Selector/Problem 02 changeFirst/app.js
+++ Query Selector/Problem 02 changeFirst/app.js
@@ -1,3 +1,7 @@
-function changeFontSize(){
-    document.querySelector(".heading1").style.fontSize = "38px";
+//assign class to that heading, which font color has to change.
+
+// by assigning class-->the font color will change of the first <h2> element
+
+function changeFirst(){
+    document.querySelector(".heading1").style.color = "red";
 }
