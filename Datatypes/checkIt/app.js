75fe37d7969ddf5052d9dfe93b55b4af851c642b
Kiranadil <kiranadil10@gmail.com>
2021-06-01 00:15:54 +0500

    checked the datatypes of variables

---
+++ Datatypes/checkIt/app.js
@@ -0,0 +1,15 @@
+let a = -1;//assign integer value
+let b = 6.9;//assign decimal value
+let c = true;//assign boolean value
+let d ="Kiran";//assign string value
+let e = null;//assign null value
+
+//check data type
+console.log(typeof a);//data type-->number
+console.log(typeof b);//data type-->number
+console.log(typeof c);//data type-->boolean
+console.log(typeof d);//data type-->string
+console.log(typeof e);//data type-->object
+
+ /*by removing quotation marks from value of variable d, it shows an error 
+   as "Kiran is not defined"*/
